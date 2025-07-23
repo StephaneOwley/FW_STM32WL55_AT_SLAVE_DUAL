94149748de4851d05f1b104d9d384b45926e5c36
StephaneOwley <stephane.loiseau@owley.io>
2025-07-23 19:18:43 +0200

    Initialisation propre du dépôt dans Sigfox_AT_Slave_DualCore

---
+++ CM0PLUS/Sigfox/App/sigfox_data.h
@@ -0,0 +1,42 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file   sigfox_data.h
+  * @author MCD Application Team
+  * @brief  provides encrypted data
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __SIGFOX_DATA_H__
+#define __SIGFOX_DATA_H__
+
+/* Includes ------------------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+//#define SIGFOX_KEY             01, 23, 45, 67, 89, AB, CD, EF, 01, 23, 45, 67, 89, AB, CD, EF
+//#define SIGFOX_ID              FE, DC, BA, 98
+//#define SIGFOX_PAC             00, 00, 00, 00, 00, 00, 00, 00
+
+/* Includes ------------------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+#define SIGFOX_KEY             A5, 81, 13, 9C, 1A, 38, A7, 32, 95, B2, 29, 59, 2C, A1, 25, 51
+#define SIGFOX_ID              03, 3E, 07, BD
+#define SIGFOX_PAC             D4, AA, E5, 29, 93, 05, 90, CE
+
+/* Exported macros -----------------------------------------------------------*/
+/* External variables --------------------------------------------------------*/
+
+#endif /*__SIGFOX_DATA_H__*/
